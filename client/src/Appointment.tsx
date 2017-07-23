ce44a479e8d46e611eea5e44b94fc4f36f1ee55d
Raigen <falkentavio@hotmail.de>
2017-07-23 15:03:29 +0200

    style: make appointments cards

--- client/src/Appointment.tsx
+++ client/src/Appointment.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react'
 
+import { Card, CardText, CardTitle, Chip } from 'material-ui'
+
 export interface AppointmentProps extends IAppointment {}
 
 export class Appointment extends React.Component<AppointmentProps, object> {
   render () {
     const {title, participants, from, to} = this.props
-    return <div>
-      <h2>{title} am {from.toLocaleDateString('de')} von {from.toLocaleTimeString('de')} bis {to.toLocaleTimeString('de')}</h2>
-      {participants.length > 0 && <ul>
-        {participants.map(participant => <p>{participant}</p>)}
-      </ul>}
-    </div>
+    return <Card>
+      <CardTitle
+        title={title}
+        subtitle={` am ${from.toLocaleDateString('de')} von ${from.toLocaleTimeString('de')} bis ${to.toLocaleTimeString('de')}`}
+      />
+      <CardText style={{display: 'flex', flexWrap: 'wrap'}}>
+        {participants.map(participant =>
+          <Chip key={participant}>{participant}</Chip>
+        )}
+      </CardText>
+    </Card>
   }
 }
